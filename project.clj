89d32599f6136bb76c4f0699d60b20379b7b0c2f
Julien Chastang <julien.c.chastang@gmail.com>
2011-11-12 21:14:12 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject meetup_11_30_2011 "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/meetup_11_30_2011/test/core.clj
@@ -0,0 +1,6 @@
+(ns meetup_11_30_2011.test.core
+  (:use [meetup_11_30_2011.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
